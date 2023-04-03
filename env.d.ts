27d06378cb922a26fe5323fce931fa30b8520a2e
David Tang <david.tang@epfl.ch>
2023-04-03 16:11:11 +0200

    feat: change colors

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="vite/client" />
+
+declare module 'vuetify/lib/util/colors'
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,6 +2,7 @@ import i18n from '@/plugins/i18n'
 import { useI18n } from 'vue-i18n'
 import { createVuetify } from 'vuetify'
 import { aliases, mdi } from 'vuetify/iconsets/mdi-svg'
+import colors from 'vuetify/lib/util/colors'
 import { createVueI18nAdapter } from 'vuetify/locale/adapters/vue-i18n'
 import 'vuetify/styles'
 
@@ -15,5 +16,15 @@ export default createVuetify({
   },
   locale: {
     adapter: createVueI18nAdapter({ i18n, useI18n })
+  },
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: colors.blue.base,
+          secondary: colors.amber.base
+        }
+      }
+    }
   }
 })
